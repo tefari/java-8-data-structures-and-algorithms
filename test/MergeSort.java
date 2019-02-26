80e704995792a538af5b1bf90cc4f7b0a54f2d9f
kortyeltefari <kortyel@tefari.com>
2019-02-26 13:48:55 -0600

    ++implemented mergeSort

--- test/MergeSort.java
+++ test/MergeSort.java
@@ -7,66 +7,65 @@ import java.util.Iterator;
 public class MergeSort extends HelperMethods {
 
 	public static void main(String[] args) {
+		mergeSort(new int [] {10,5,4,3});
 
 	}
 
 	public static int[] mergeSort(int[] array) {
-		return null;
+		int n = array.length;
+		int[] first;
+		int[] second;
+
+		//
+		if (n % 2 == 0) {
+			first = new int[n / 2];
+			second = new int[n / 2];
+		} else {
+			first = new int[n / 2];
+			second = new int[n / 2 + 1];
+		}
+
+		//
+		for (int i = 0; i < n; i++) {
+			if (i < n / 2) {
+				first[i] = array[i];
+			} else {
+				second[i - n / 2] = array[i];
+			}
+		}
+
+		//
+		first = mergeSort(first);
+		second = mergeSort(second);
+
+		return merge(first, second);
 	}
 
 	public static int[] merge(int[] first, int[] second) {
-		return null;
-	}
+		int[] result = new int[first.length + second.length];
 
-	public static void test() {
-		int[] array = { 1, 2, 3, 4, 5, 6, 7, 8 };
-		int[][] array2 = new int[8][8];
+		//
+		int i = 0;
+		int j = 0;
+		int index = 0;
 
-		int timesHalved = 0;
-		while (halve(array)[0].length >= 2) {
-			int h = halve(array)[0].length;
-			array = halve(array)[0];
-			timesHalved++;
-
-//			int h[][] = new int[array.length][array.length/2];
-//			
-//			for (int i = 0; i < array.length; i++) {
-//				int [][] ha = halve(array);
-//				
-//				for(int hai=0;hai<ha.length;hai++) {
-//					int[][] t = halve(ha[hai]);
-//					for(int ti = 0; ti<t.length;ti++) {
-//						
-//					}
-//				}
-//				
-//				int index = 0;
-//				for (int hi = 0; hi < array.length/2; hi++) {
-//					h[i][hi] =ha[i][index];
-//					 ++index;
-//				}
-			//
-//			}
+		//
+		while (i < first.length && j < second.length) {
+			if (first[i] < second[j]) {
+				result[index++] = first[i++];
+			} else {
+				result[index++] = second[j++];
+			}
 		}
 
-	}
+		//
+		while (i < first.length) {
+			result[index++] = first[i++];
+		}
 
-	public static int[][] halve(int[] array) {
-
-		int length = array.length;
-		int index = 0;
-		int[][] result = new int[2][length / 2];
-
-		for (int i = 0; i < length; i++) {
-			// System.out.println(array[i]);
-			if (i < length / 2) {
-				// System.out.println("1:" + array[i]);
-				result[0][i] = array[i];
-			} else {
-				// System.out.println("2:" + array[i]);
-				result[1][index] = array[i];
-				index++;
-			}
+		//
+		while (j < second.length) {
+			result[index++] = second[j++];
 		}
 
 		return result;
